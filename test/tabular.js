68895c73437bfa18a019bde1761c27438eba781c
Poga Po <poga.bahamut@gmail.com>
2016-02-20 19:20:05 +0800

    fix insertRowAfter

--- test/tabular.js
+++ test/tabular.js
@@ -4,7 +4,10 @@ let expect = require('chai').expect
 let Tabular = require("../lib/tabular").Tabular
 
 describe('Tabular', () => {
-    let t = new Tabular()
+    let t = null
+    beforeEach(() => {
+        t = new Tabular()
+    })
 
     describe('#constructor()', () => {
         it('should initialize everything properly', () => {
@@ -20,15 +23,28 @@ describe('Tabular', () => {
             t.appendRow([1,2,3])
 
             expect(t.rows).have.length(1)
-            expect(t.header).equal(null)
-            expect(t.rowCount).equal(1)
-            expect(t.colCount).equal(3)
             expect(t.size()).deep.equal({row: 1, col: 3})
         })
         it('should throw exception if row doesn\'t have same size', () => {
             t.appendRow([1,2,3])
+
             expect(function() {
                 t.appendRow([1,2])
+            }).to.throw("Incorrect row size")
+        })
+    })
+    describe('#insertRowAfter()', () => {
+        it('should append new row and update tabular size', () => {
+            t.insertRowAfter(0,[1,2,3])
+
+            expect(t.rows).have.length(1)
+            expect(t.size()).deep.equal({row: 1, col: 3})
+        })
+        it('should throw exception if row doesn\'t have same size', () => {
+            t.insertRowAfter(0, [1,2,3])
+
+            expect(function() {
+                t.insertRowAfter(0, [1,2])
             }).to.throw("Incorrect row size")
         })
     })
