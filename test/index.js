03ee8baaeecd2270d6b4fa63c56da9604cc71c26
Poga Po <poga.bahamut@gmail.com>
2016-02-22 23:20:43 +0800

    more test

--- test/index.js
+++ test/index.js
@@ -20,4 +20,32 @@ describe('postabular', () => {
 
         fs.createReadStream(`${__dirname}/test.csv`).pipe(postabular()).pipe(ws)
     })
+
+    it('should allow plugins to modify origin tabular data', (done) => {
+        let ws = stream.Writable()
+        let result = ""
+        ws._write = function(chunk, enc, next) {
+            result += chunk.toString()
+            next()
+        }
+        ws.on("finish", () => {
+            expect(result).equal("h1,h2,h3\n1,1,1\n4,5,6\n")
+            done()
+        })
+        fs.createReadStream(`${__dirname}/test.csv`).pipe(postabular([require("../example/set_first_row_to_1")])).pipe(ws)
+    })
+
+    it('should allow plugins to return a new tabular data', (done) => {
+        let ws = stream.Writable()
+        let result = ""
+        ws._write = function(chunk, enc, next) {
+            result += chunk.toString()
+            next()
+        }
+        ws.on("finish", () => {
+            expect(result).equal("a,b,c\n1,2,3\n")
+            done()
+        })
+        fs.createReadStream(`${__dirname}/test.csv`).pipe(postabular([require("../example/replace")])).pipe(ws)
+    })
 })
